cc6713e331b1844a80dd5cba19150514f3d3c792
eldarcodes <prog.eldar@gmail.com>
2024-03-06 18:44:38 +0200

    test: basic locators examples

--- tests/first-test.spec.ts
+++ tests/first-test.spec.ts
@@ -1,14 +1,66 @@
-import { test } from "@playwright/test";
+import { test, expect } from "@playwright/test";
 
 test.beforeEach(async ({ page }) => {
   await page.goto("http://localhost:4200/");
   await page.getByText("Forms").click();
-});
-
-test("navigate to form layouts page", async ({ page }) => {
   await page.getByText("Form Layouts").click();
 });
 
-test("navigate to datepicker page", async ({ page }) => {
-  await page.getByText("Datepicker").click();
+test("Locator syntax rules", async ({ page }) => {
+  await page.locator("input").first().click();
+});
+
+test("User facing locators", async ({ page }) => {
+  await page.getByLabel("Email").first().click();
+  await page.getByPlaceholder("Jane Doe").click();
+});
+
+test("Locating child elements", async ({ page }) => {
+  await page.locator("nb-card nb-radio").getByText("Option 1").click();
+});
+
+test("Locating parent elements", async ({ page }) => {
+  await page
+    .locator("nb-card", { hasText: "Using the Grid" })
+    .getByRole("textbox", { name: "Email" })
+    .click();
+
+  await page
+    .locator("nb-card", { has: page.locator("#inputEmail1") })
+    .getByRole("textbox", { name: "Email" })
+    .click();
+
+  await page
+    .locator("nb-card")
+    .filter({ hasText: "Basic form" })
+    .getByRole("textbox", { name: "Email" })
+    .click();
+
+  await page
+    .locator("nb-card")
+    .filter({ has: page.locator(".status-danger") })
+    .getByRole("textbox", { name: "Password" })
+    .click();
+
+  await page
+    .locator("nb-card")
+    .filter({ has: page.locator("nb-checkbox") })
+    .filter({ hasText: "Sign in" })
+    .getByRole("textbox", { name: "Password" })
+    .click();
+});
+
+test("Reusing locators", async ({ page }) => {
+  const basicForm = await page
+    .locator("nb-card")
+    .filter({ hasText: "Basic form" });
+
+  const emailField = basicForm.getByRole("textbox", { name: "Email" });
+
+  await emailField.fill("me@acme.com");
+  await basicForm.getByRole("textbox", { name: "Password" }).fill("123");
+  await basicForm.locator("nb-checkbox").click();
+  await basicForm.getByRole("button").click();
+
+  await expect(emailField).toHaveValue("me@acme.com");
 });
