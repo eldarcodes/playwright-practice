269a74dc4e388e3614c103840f2877481303fb34
eldarcodes <prog.eldar@gmail.com>
2024-03-13 19:20:34 +0200

    test: intercept browser api response

--- tests/api.spec.ts
+++ tests/api.spec.ts
@@ -82,3 +82,63 @@ test("Delete article", async ({ page, request }) => {
     "Eldar test article title"
   );
 });
+
+test("Create article", async ({ page, request }) => {
+  await page.getByText("New Article").click();
+
+  await page
+    .getByRole("textbox", { name: "Article Title" })
+    .fill("Playwright test article title");
+  await page
+    .getByRole("textbox", { name: "What's this article about?" })
+    .fill("About playwright");
+  await page
+    .getByRole("textbox", { name: "Write your article (in markdown)" })
+    .fill("Playwright test article body");
+
+  await page.waitForTimeout(3000);
+  await page.getByRole("button", { name: "Publish Article" }).click();
+
+  const responseArticle = await page.waitForResponse(
+    "https://api.realworld.io/api/articles/"
+  );
+  const responseArticleBody = await responseArticle.json();
+  const articleSlug = responseArticleBody.article.slug;
+
+  await expect(page.locator("app-article-page h1").first()).toContainText(
+    "Playwright test article title"
+  );
+
+  await page.getByText("Home").click();
+  await page.getByText("Global Feed").click();
+
+  await expect(page.locator("app-article-list h1").first()).toContainText(
+    "Playwright test article title"
+  );
+
+  const response = await request.post(
+    "https://api.realworld.io/api/users/login",
+    {
+      data: {
+        user: {
+          email: "eldar@eldar.com",
+          password: "eldar",
+        },
+      },
+    }
+  );
+
+  const responseBody = await response.json();
+  const accessToken = responseBody.user.token;
+
+  const articleDeleteResponse = await request.delete(
+    `https://api.realworld.io/api/articles/${articleSlug}`,
+    {
+      headers: {
+        Authorization: `Token ${accessToken}`,
+      },
+    }
+  );
+
+  expect(articleDeleteResponse.status()).toBe(204);
+});
