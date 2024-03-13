f3d204ac2641f7d9d648aa4033b73169d9f072e9
eldarcodes <prog.eldar@gmail.com>
2024-03-13 19:07:36 +0200

    test: creating and deleting articles, authorization

--- tests/api.spec.ts
+++ tests/api.spec.ts
@@ -8,27 +8,77 @@ test.beforeEach(async ({ page }) => {
     });
   });
 
+  await page.goto("https://angular.realworld.how/");
+
+  await page.getByText("Sign in").click();
+  await page.getByRole("textbox", { name: "Email" }).fill("eldar@eldar.com");
+  await page.getByRole("textbox", { name: "Password" }).fill("eldar");
+  await page.getByRole("button").click();
+});
+
+test("Has title", async ({ page }) => {
   await page.route("*/**/api/articles*", async (route) => {
     const response = await route.fetch();
     const responseBody = await response.json();
-    responseBody.articles[0].title = "This is updated title";
-    responseBody.articles[0].description = "This is updated description";
+    responseBody.articles[0].title = "This is updated MOCK title";
+    responseBody.articles[0].description = "This is updated MOCK description";
 
     await route.fulfill({
       body: JSON.stringify(responseBody),
     });
   });
 
-  await page.goto("https://angular.realworld.how/");
-});
+  await page.getByText("Global Feed").click();
 
-test("Has title", async ({ page }) => {
   await expect(page.locator(".navbar-brand")).toHaveText("conduit");
-
   await expect(page.locator("app-article-list h1").first()).toContainText(
-    "This is updated title"
+    "This is updated MOCK title"
   );
   await expect(page.locator("app-article-list p").first()).toContainText(
-    "This is updated description"
+    "This is updated MOCK description"
+  );
+});
+
+test("Delete article", async ({ page, request }) => {
+  const response = await request.post(
+    "https://api.realworld.io/api/users/login",
+    {
+      data: {
+        user: {
+          email: "eldar@eldar.com",
+          password: "eldar",
+        },
+      },
+    }
+  );
+
+  const responseBody = await response.json();
+  const accessToken = responseBody.user.token;
+
+  const responseArticle = await request.post(
+    "https://api.realworld.io/api/articles",
+    {
+      data: {
+        article: {
+          title: "Eldar test article title",
+          description: "Eldar test article description",
+          body: "Eldar test article body",
+          tagList: ["Eldar test article tag"],
+        },
+      },
+      headers: {
+        Authorization: `Token ${accessToken}`,
+      },
+    }
+  );
+
+  expect(responseArticle.status()).toBe(201);
+
+  await page.getByText("Global Feed").click();
+  await page.getByText("Eldar test article title").click();
+  await page.getByRole("button", { name: "Delete Article" }).first().click();
+
+  await expect(page.locator("app-article-list h1").first()).toContainText(
+    "Eldar test article title"
   );
 });
